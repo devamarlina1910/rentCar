eca332256e4c7b3df882f02601d4afc52850967c
Deva Marlina <devamarlina.1019@gmail.com>
2024-11-12 07:08:45 +0700

    Tubes

---
+++ app/Filament/Widgets/UserChart.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+use App\Models\Post;
+
+class UserChart extends ChartWidget
+{
+    protected static ?string $heading = 'Posts Chart';
+
+    protected function getData(): array
+    {
+        $data = Trend::model(Post::class)
+        ->between(
+            start: now()->startOfYear(),
+            end: now()->endOfYear(),
+        )
+        ->perMonth()
+        ->count();
+ 
+    return [
+        'datasets' => [
+            [
+                'label' => 'Blog posts',
+                'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+            ],
+        ],
+        'labels' => $data->map(fn (TrendValue $value) => $value->date),
+    ];
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
